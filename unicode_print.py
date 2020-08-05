fbb03666e9e7096698196d0b7ce5ad7fb54dd785
Shoeb Joarder <shoebjoarder@outlook.com>
2020-08-05 08:53:58 +0200

    removed files

--- unicode_print.py
+++
@@ -1,3 +0,0 @@
-text = "Brauchen ein gut ausgestattetes Milit\u00e4r - aber keine Aufr\u00fcstung \u00e0 la Trump! Geld, das Frau Merkel f\u00fcr Aufr\u00fcstung will, muss in die Bildung!"
-
-print("\n" + text.encode('utf-8','surrogatepass').decode('utf-8') + "\n")
